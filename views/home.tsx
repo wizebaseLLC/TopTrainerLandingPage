c4a63917033ee4a2633a74917826061d269fc099
servon <servonlewis@gmail.com>
2022-07-10 22:11:33 -0400

    fixed build

--- views/home.tsx
+++ views/home.tsx
@@ -9,7 +9,7 @@ import {
 export const HomePage = () => {
   return (
     <HStack w="100vw">
-      <Flex w="100%" h="100vh" minH={1117} align="center" justify="center">
+      <Flex w="100%" h="100vh" align="center" justify="center">
         <VStack>
           <Image
             src="/images/Top-trainer-1.png"
@@ -43,7 +43,6 @@ export const HomePage = () => {
       <Box
         w={{ base: 0, md: "100%" }}
         h="100vh"
-        minH={1117}
         boxShadow="inset 30px 30px 90px rgba(0, 0, 0, 0.6)"
         backgroundImage="/images/rightSideContainer.png"
         backgroundRepeat="no-repeat"
